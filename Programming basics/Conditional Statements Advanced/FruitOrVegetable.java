fa25571329c4c7b4e0ba5351d4facf88952e5b63
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-09-23 08:08:26 +0300

    Add files via upload

---
+++ Programming basics/Conditional Statements Advanced/FruitOrVegetable.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FruitOrVegetable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fruit = scanner.nextLine();
+
+        switch (fruit) {
+            case "banana":
+            case "apple":
+            case "kiwi":
+            case "cherry":
+            case "lemon":
+            case "grapes":
+                System.out.println("fruit");
+                break;
+            case "tomato":
+            case "cucumber":
+            case "pepper":
+            case "carrot":
+                System.out.println("vegetable");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
