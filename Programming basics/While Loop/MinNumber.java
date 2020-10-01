9d8c357da22a5ee1ef531ba0549f2148cade0bd6
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-01 08:25:29 +0300

    Add files via upload

---
+++ Programming basics/While Loop/MinNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MinNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int minNum = Integer.MAX_VALUE;
+
+        while (!input.equals("Stop")) {
+            int currentNum = Integer.parseInt(input);
+            if (currentNum < minNum) {
+                minNum = currentNum;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println(minNum);
+    }
+}
