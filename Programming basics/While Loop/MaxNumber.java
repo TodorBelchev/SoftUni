bad89c2a71f6b2b4e64b3ee4bcd36485f631d6a1
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-01 08:24:53 +0300

    Add files via upload

---
+++ Programming basics/While Loop/MaxNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MaxNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int maxNum = Integer.MIN_VALUE;
+
+        while (!input.equals("Stop")) {
+            int currentNum = Integer.parseInt(input);
+            if (currentNum > maxNum) {
+                maxNum = currentNum;
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println(maxNum);
+    }
+}
