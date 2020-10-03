cb54b210bc7ad01365c2b51ea40f3573d0f2cc09
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-03 16:37:42 +0300

    Add files via upload

---
+++ Programming basics/While Loop/NumbersDividedByThreeWithoutReminder.java
@@ -0,0 +1,7 @@
+public class NumbersDividedByThreeWithoutReminder {
+    public static void main(String[] args) {
+        for (int i = 3; i < 100; i += 3) {
+            System.out.println(i);
+        }
+    }
+}
