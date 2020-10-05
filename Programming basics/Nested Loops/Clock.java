a883a6ec0e5f5353dc9f9fecd7b0d7e7f7db03c7
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-05 08:52:07 +0300

    Create Clock.java

---
+++ Programming basics/Nested Loops/Clock.java
@@ -0,0 +1,9 @@
+public class Clock {
+    public static void main(String[] args) {
+        for (int hour = 0; hour < 24; hour++) {
+            for (int minutes = 0; minutes < 60; minutes++) {
+                System.out.printf("%d:%d%n", hour, minutes);
+            }
+        }
+    }
+}
