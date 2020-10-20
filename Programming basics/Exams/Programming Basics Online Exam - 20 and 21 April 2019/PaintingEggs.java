435f5e1417f08775993acb98f6030c5b9be67c08
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-20 11:52:13 +0300

    Add files via upload

---
+++ Programming basics/Exams/Programming Basics Online Exam - 20 and 21 April 2019/PaintingEggs.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class PaintingEggs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String size = scanner.nextLine();
+        String color = scanner.nextLine();
+        int eggs = Integer.parseInt(scanner.nextLine());
+        int price = 0;
+
+        if (size.equals("Large")) {
+
+            if (color.equals("Red")) {
+                price = 16;
+            } else if (color.equals("Green")) {
+                price = 12;
+            } else if (color.equals("Yellow")) {
+                price = 9;
+            }
+
+        } else if (size.equals("Medium")) {
+
+            if (color.equals("Red")) {
+                price = 13;
+            } else if (color.equals("Green")) {
+                price = 9;
+            } else if (color.equals("Yellow")) {
+                price = 7;
+            }
+
+        } else if (size.equals("Small")) {
+
+            if (color.equals("Red")) {
+                price = 9;
+            } else if (color.equals("Green")) {
+                price = 8;
+            } else if (color.equals("Yellow")) {
+                price = 5;
+            }
+
+        }
+
+        double total = (eggs * price) - (eggs * price * 0.35);
+        System.out.printf("%.2f leva.", total);
+    }
+}
