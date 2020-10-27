03f657561fad5d723c4423f0f5a82a0d25e4c7c7
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-27 08:31:02 +0200

    Add files via upload

---
+++ Programming fundamentals/Basic Syntax, Conditional Statements and Loops/ForeignLanguages.js
@@ -0,0 +1,16 @@
+function solve(country) {
+    switch (country) {
+        case `USA`:
+        case `England`:
+            console.log(`English`);
+            break;
+        case `Spain`:
+        case `Argentina`:
+        case `Mexico`:
+            console.log(`Spanish`);
+            break;
+        default:
+            console.log(`unknown`);
+            break;
+    }
+}
