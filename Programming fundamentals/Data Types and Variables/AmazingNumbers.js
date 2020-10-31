70d8978733881d3e73b40ff752b96fb78a49dab7
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-10-31 08:52:34 +0200

    Add files via upload

---
+++ Programming fundamentals/Data Types and Variables/AmazingNumbers.js
@@ -0,0 +1,11 @@
+function solve(num) {
+    num = num.toString();
+    let sum = 0;
+    
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+    let result = sum.toString().includes(`9`);
+    console.log(result ? `${num} Amazing? True` : `${num} Amazing? False`);
+
+}
