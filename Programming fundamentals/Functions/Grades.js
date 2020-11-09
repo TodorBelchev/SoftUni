d47655c3952da1122494c56192ff0028d5333684
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-11-09 14:34:00 +0200

    Update Grades.js

--- Programming fundamentals/Functions/Grades.js
+++ Programming fundamentals/Functions/Grades.js
@@ -1,15 +1,15 @@
 function solve(grade) {
     let result = ``;
     if (grade >= 2 && grade < 3) {
-        result = `Fail`;
+        result = `Fail (${grade.toFixed(0)})`;
     } else if (grade >= 3 && grade < 3.5) {
-        result = `Poor`;
+        result = `Poor (${grade.toFixed(2)})`;
     } else if (grade >= 3.5 && grade < 4.5) {
-        result = `Good`;
+        result = `Good (${grade.toFixed(2)})`;
     } else if (grade >= 4.5 && grade < 5.5) {
-        result = `Very good`;
+        result = `Very good (${grade.toFixed(2)})`;
     } else if (grade >= 5.5 && grade <= 6) {
-        result = `Excellent`;
+        result = `Excellent (${grade.toFixed(2)})`;
     }
     console.log(result);
 }
