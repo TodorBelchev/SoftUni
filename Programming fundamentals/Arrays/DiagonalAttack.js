635501d78744caf14fde71e5ceed03fd256db459
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-11-09 14:26:45 +0200

    Add files via upload

---
+++ Programming fundamentals/Arrays/DiagonalAttack.js
@@ -0,0 +1,33 @@
+function solve(input) {
+    const matrix = [];
+
+    for (const line of input) {
+        matrix.push(line.split(' ').map(Number));
+    }
+
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        leftSum += matrix[i][i];
+        rightSum += matrix[matrix[i].length - 1 - i][matrix[i].length - 1 - i];
+    }
+
+    if (leftSum === rightSum) {
+
+        for (let i = 0; i < matrix[0].length; i++) {
+
+            for (let j = 0; j < matrix[0].length; j++) {
+
+                if (j !== i && j !== matrix[i].length - 1 - i) {
+                    matrix[i][j] = leftSum;
+                }
+
+            }
+
+        }
+        
+    }
+    
+    matrix.forEach(row => console.log(row.join(' ')));
+}
