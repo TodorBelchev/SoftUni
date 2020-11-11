22954c68e9895308cc3a56ab29172119eec1c694
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-11-11 07:40:33 +0200

    Add files via upload

---
+++ Programming fundamentals/Functions/NxNMatrix.js
@@ -0,0 +1,9 @@
+function solve(num) {
+    for (let i = 0; i < num; i++) {
+        let line = ``;
+        for (let j = 0; j < num; j++) {
+            line += num + ` `;
+        }
+        console.log(line);
+    }
+}
