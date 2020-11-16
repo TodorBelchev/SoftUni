8b648d79962d70f8ae3fb5b2b886962c77694b2b
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-11-16 08:14:49 +0200

    Add files via upload

---
+++ Programming fundamentals/Arrays Advanced/EqualNeighbors.js
@@ -0,0 +1,31 @@
+function solve(input) {
+    let counter = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        const current = input[i];
+        const next = input[i + 1] || [];
+
+        for (let j = 0; j < current.length; j++) {
+
+            if (current[j] === next[j]) {
+                counter++;
+            }
+
+            if (current[j] === current[j + 1]) {
+                counter++;
+            }
+
+        }
+    }
+
+    console.log(counter);
+}
+
+solve(
+    [
+        ['2', '3', '4', '7', '0'],
+        ['4', '0', '5', '3', '4'],
+        ['2', '3', '5', '4', '2'],
+        ['9', '8', '7', '5', '4']
+    ]
+)
