6ae7d5812d3ca6dca4b6636aee8ea845bda200b9
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2020-11-18 08:12:55 +0200

    Add files via upload

---
+++ Programming fundamentals/Objects and Classes/ConvertToJSON.js
@@ -0,0 +1,14 @@
+function solve(name, lastName, hairColor) {
+    let person = {
+        name,
+        lastName,
+        hairColor
+    }
+    console.log(JSON.stringify(person));
+}
+
+solve(
+    'George',
+    'Jones',
+    'Brown'
+)
