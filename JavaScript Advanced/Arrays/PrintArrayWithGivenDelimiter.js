fd6e05ff68eeface633bcca29daeb9f045d3304e
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-01-29 08:46:14 +0200

    Add files via upload

---
+++ JavaScript Advanced/Arrays/PrintArrayWithGivenDelimiter.js
@@ -0,0 +1,14 @@
+function solve(input, delimiter) {
+    console.log(input.join(delimiter));
+}
+
+solve(
+    [
+        'One',
+        'Two',
+        'Three',
+        'Four',
+        'Five',
+    ],
+    '-'
+)
