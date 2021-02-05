13ff61ec58c876c265c3877b7f2e21c24d8d7113
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-02-05 08:36:08 +0200

    Delete JavaScript Advanced/Objects and Classes directory

--- JavaScript Advanced/Objects and Classes/biggerHalf.js
+++
@@ -1,5 +0,0 @@
-function solve(input) {
-    return input.sort((a, b) => a - b).slice(input.length / 2);
-}
-
-console.log(solve([3, 19, 14, 7, 2, 19, 6]));
--- JavaScript Advanced/Objects and Classes/listOfNames.js
+++
@@ -1,5 +0,0 @@
-function solve(input) {
-    return input.sort().map((x, index) => x = `${index + 1}.${x}`).join('\n');
-}
-
-console.log(solve(["John", "Bob", "Christina", "Ema"]));
