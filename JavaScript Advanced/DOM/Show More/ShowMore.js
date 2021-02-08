de79c41e81050d8ad5115cb89d943cb830a5df93
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-02-08 09:00:36 +0200

    Add files via upload

---
+++ JavaScript Advanced/DOM/Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const btn = document.querySelector('#more');
+    const span = document.querySelector('#text');
+    span.style.display = 'inline';
+    btn.style.display = 'none';
+}
