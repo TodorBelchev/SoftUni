53cf10c117be9bd36e74ac48fd7c14932138466e
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-02-17 08:20:08 +0200

    Add files via upload

---
+++ JavaScript Advanced/DOM manipulations/Notification V2/app.js
@@ -0,0 +1,6 @@
+function notify(message) {
+  const notification = document.getElementById('notification');
+  notification.textContent = message;
+  notification.style.display = 'block';
+  notification.addEventListener('click', () => notification.style.display = 'none');
+}
