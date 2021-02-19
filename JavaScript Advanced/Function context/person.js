f158765d300a5c31915e67417f73c83268f50bd0
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-02-19 08:21:33 +0200

    Add files via upload

---
+++ JavaScript Advanced/Function context/person.js
@@ -0,0 +1,29 @@
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    set fullName(full) {
+        if (/\w \w/.test(full)) {
+            const [first, last] = full.split(' ');
+            this.firstName = first;
+            this.lastName = last;
+        }
+    }
+}
+
+
+let person = new Person("Peter", "Ivanov");
+console.log(person.fullName);//Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName);//George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName);//George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName)//Nikola
+console.log(person.lastName)//Tesla
