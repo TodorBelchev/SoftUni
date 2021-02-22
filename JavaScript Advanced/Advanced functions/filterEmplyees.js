8cc85aa89455a4f13b9097bd263868e3a76aff90
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-02-22 09:26:57 +0200

    Add files via upload

---
+++ JavaScript Advanced/Advanced functions/filterEmplyees.js
@@ -0,0 +1,34 @@
+function solve(data, criteria) {
+    const arr = JSON.parse(data);
+    const [key, value] = criteria.split('-');
+    const filtered = arr.filter(x => x[key] === value);
+    let counter = 0;
+    filtered.forEach(x => console.log(`${counter++}. ${x.first_name} ${x.last_name} - ${x.email}`))
+}
+
+solve(`[{
+    "id": "1",
+    "first_name": "Kaylee",
+    "last_name": "Johnson",
+    "email": "k0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Johnson",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  }, {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }, {
+    "id": "4",
+    "first_name": "Evanne",
+    "last_name": "Johnson",
+    "email": "ev2@hostgator.com",
+    "gender": "Male"
+  }]`,
+ 'last_name-Johnson')
