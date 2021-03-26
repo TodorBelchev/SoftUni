64e130e9ab7595514e6e17dcf5514ae4dedf2252
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-03-26 12:48:46 +0200

    Add files via upload

---
+++ JavaScript Applications/Routing/Furniture/views/loginView.js
@@ -0,0 +1,29 @@
+import { login } from '../controllers/login.js';
+import { html } from '../node_modules/lit-html/lit-html.js';
+import { navigationView } from './navigationView.js';
+
+export const loginView = (ctx) => html`
+    ${navigationView(ctx)}
+    <div class="container">
+        <div class="row space-top">
+            <div class="col-md-12">
+                <h1>Login User</h1>
+                <p>Please fill all fields.</p>
+            </div>
+        </div>
+        <form @submit=${login}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="email">Email</label>
+                        <input class="form-control" id="email" type="text" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="password">Password</label>
+                        <input class="form-control" id="password" type="password" name="password">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Login" />
+                </div>
+            </div>
+        </form>
+    </div>`;
