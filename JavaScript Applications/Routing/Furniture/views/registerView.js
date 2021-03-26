fc26f38491a28abdc3cd2dc29d59768ad6d8335b
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-03-26 12:49:30 +0200

    Add files via upload

---
+++ JavaScript Applications/Routing/Furniture/views/registerView.js
@@ -0,0 +1,33 @@
+import { html } from '../node_modules/lit-html/lit-html.js';
+import { navigationView } from './navigationView.js';
+import { register } from '../controllers/register.js';
+
+export const registerView = (ctx) => html`
+    ${navigationView(ctx)}
+    <div class="container">
+        <div class="row space-top">
+            <div class="col-md-12">
+                <h1>Register New User</h1>
+                <p>Please fill all fields.</p>
+            </div>
+        </div>
+        <form @submit=${register}>
+            <div class="row space-top">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label class="form-control-label" for="email">Email</label>
+                        <input class="form-control" id="email" type="text" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="password">Password</label>
+                        <input class="form-control" id="password" type="password" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-control-label" for="rePass">Repeat</label>
+                        <input class="form-control" id="rePass" type="password" name="rePass">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Register" />
+                </div>
+            </div>
+        </form>
+    </div>`;
