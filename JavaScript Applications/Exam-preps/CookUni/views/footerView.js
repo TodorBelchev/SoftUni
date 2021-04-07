30060740d2fd6fa35d6ea7d7d66e5fd7fec739fd
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-04-07 08:22:11 +0300

    Add files via upload

---
+++ JavaScript Applications/Exam-preps/CookUni/views/footerView.js
@@ -0,0 +1,9 @@
+import { html } from '../node_modules/lit-html/lit-html.js';
+
+export const footerView = () => html`
+    <footer class="mastfoot mt-5 text-center text-light">
+        <div class="inner">
+            <p>Made with &hearts; by <a href="#/home">CookUni</a>.</p>
+        </div>
+        <i class="fa fa-heart"></i>
+    </footer>`;
