b5cfc29fdfffd8fb489d3eeea67a075ca1b3f6be
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-04-12 09:05:52 +0300

    Add files via upload

---
+++ JavaScript Applications/Exam-preps/ShoeShelf/views/footer.js
@@ -0,0 +1,6 @@
+import { html } from '../node_modules/lit-html/lit-html.js';
+
+export const footerView = () => html`
+<footer>
+    <p><a href="https://softuni.bg">Software University</a> - JS Applications @ 2020</p>
+</footer>`;
