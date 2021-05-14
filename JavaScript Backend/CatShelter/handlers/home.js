73444c885d87256211a8d6c633ec84579d46d2ae
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-14 09:13:43 +0300

    Added gitignore

---
+++ JavaScript Backend/CatShelter/handlers/home.js
@@ -0,0 +1,8 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const cats = require('../data/cats.json');
+
+module.exports = (req, res) => {
+    console.log(req);
+}
--- JavaScript Backend/CatShelter/index.js
+++ JavaScript Backend/CatShelter/index.js
@@ -2,9 +2,5 @@ const http = require('http');
 const port = 3000;
 
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('hello world');
-    res.end();
+    console.log(req.url);
 }).listen(port);
