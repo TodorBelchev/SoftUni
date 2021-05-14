fc46d755d141de9305c2e96ad5fd17e2463d08d2
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-14 10:03:50 +0300

    Added home view

--- JavaScript Backend/CatShelter/index.js
+++ JavaScript Backend/CatShelter/index.js
@@ -1,6 +1,29 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 const port = 3000;
 
 http.createServer((req, res) => {
-    console.log(req.url);
+    let filePath = path.join(__dirname, req.url === '/' ? 'views' : '', req.url === '/' ? 'index.html' : req.url);
+    let extName = path.extname(filePath);
+
+    const contentTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.ico': 'image/x-icon'
+    }
+    let contentType = contentTypes[extName];
+
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+        if (err) {
+            console.log(err);
+        }
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content);
+    });
+
 }).listen(port);
