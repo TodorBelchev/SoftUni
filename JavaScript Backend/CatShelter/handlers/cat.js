9a301e6e0107df47c8f2ae12496d4917eec70d6d
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-17 09:26:04 +0300

    Add breed post request handler

--- JavaScript Backend/CatShelter/handlers/cat.js
+++ JavaScript Backend/CatShelter/handlers/cat.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const qs = require('querystring');
 const { handleHtmlFileRead } = require('../utils');
 
 module.exports = (req, res) => {
@@ -14,6 +15,31 @@ module.exports = (req, res) => {
     } else if (pathName === '/cats/add-breed' && req.method === 'GET') {
         filePath = path.normalize(path.join(__dirname, '../views/addBreed.html'));
         fs.readFile(filePath, (err, data) => handleHtmlFileRead(err, data, res));
+    } else if (pathName === '/cats/add-breed' && req.method === 'POST') {
+        let formData = '';
+        req.on('data', data => formData += data);
+        req.on('end', () => {
+            const body = qs.parse(formData);
+
+            fs.readFile('./data/breeds.json', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+
+                const breeds = JSON.parse(data);
+                breeds.push(body.breed);
+
+                fs.writeFile('./data/breeds.json', JSON.stringify(breeds), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log('File saved');
+                });
+
+                res.writeHead(302, { location: '/' });
+                res.end();
+            });
+        });
     } else {
         return true;
     }
