cde28722f9e6840b4d7226a67f8385eea35d6366
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-24 10:42:14 +0300

    Added attach accessory

--- JavaScript Backend/Cubicle/controllers/cubeController.js
+++ JavaScript Backend/Cubicle/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const Cube = require('../models/Cube');
+const Accessory = require('../models/Accessory');
 
 const router = Router();
 
@@ -16,8 +17,24 @@ router.post('/create', (req, res) => {
 });
 
 router.get('/details/:id', async (req, res) => {
-    const cube = await Cube.findById(req.params.id).lean();
+    const cube = await Cube.findById(req.params.id)
+        .populate('accessories')
+        .lean();
     res.status(200).render('details', cube);
+});
+
+router.get('/attach/:id', async (req, res) => {
+    const cube = await Cube.findById(req.params.id).lean();
+    const accessories = await Accessory.find({ _id: {$nin: cube.accessories} }).lean();
+    res.status(200).render('attachAccessory', { cube, accessories });
+});
+
+router.post('/attach/:id', async (req, res) => {
+    const cube = await Cube.findById(req.params.id);
+    const accessory = await Accessory.findById(req.body.accessory);
+    cube.accessories.push(accessory);
+    cube.save();
+    res.status(200).redirect('/cube/details/' + req.params.id);
 });
 
 module.exports = router;
