b663d02ed1934e7162c992dab9e983ebbc0d9915
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-26 13:03:49 +0300

    Added solutions for 13-18 tasks

--- MySQL/Data Definition And Data Types/exercise.sql
+++ MySQL/Data Definition And Data Types/exercise.sql
@@ -282,3 +282,71 @@ VALUES
 (1, 1, 1, 'New','2000-01-01'),
 (1, 1, 1, 'New','2000-01-01'),
 (1, 1, 1, 'New', '2000-01-01');
+
+-- 13. Basic Insert
+
+INSERT INTO `towns`
+(`name`)
+VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+INSERT INTO `departments`
+(`name`)
+VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+INSERT INTO `employees`
+(`first_name`,`middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
+VALUES
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
+
+-- 14. Basic Select All Fields
+
+SELECT * FROM `towns`;
+SELECT * FROM `departments`;
+SELECT * FROM `employees`;
+
+-- 15. Basic Select All Fields and Order Them
+
+SELECT * FROM `towns`
+ORDER BY `name` ASC;
+
+SELECT * FROM `departments`
+ORDER BY `name` ASC;
+
+SELECT * FROM `employees`
+ORDER BY `salary` DESC;
+
+-- 16. Basic Select Some Fields
+
+SELECT `name` FROM `towns`
+ORDER BY `name` ASC;
+
+SELECT `name` FROM `departments`
+ORDER BY `name` ASC;
+
+SELECT `first_name`, `last_name`, `job_title`, `salary` FROM `employees`
+ORDER BY `salary` DESC;
+
+-- 17. Increase Employees Salary
+
+UPDATE `employees` 
+SET `salary` = `salary` * 1.1
+WHERE `id` > 0;
+
+SELECT `salary` FROM `employees`;
+
+-- 18. Delete All Records
+
+DELETE FROM `occupancies`;
