f8cd3d8fae0d7620c69cd750f1da87e8263ca139
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-27 17:56:04 +0300

    Added solution

--- MySQL/Basic CRUD/exercise.sql
+++ MySQL/Basic CRUD/exercise.sql
@@ -98,3 +98,24 @@ SET `salary` = `salary` * 1.12
 WHERE `department_id` IN(1, 2, 4, 11);
 
 SELECT `salary` FROM `employees`;
+
+-- 21. All Mountain Peaks
+SELECT `peak_name` FROM `peaks`
+ORDER BY `peak_name` ASC;
+
+-- 22. Biggest Countries by Population
+SELECT `country_name`, `population`
+FROM `countries`
+WHERE `continent_code` = 'EU'
+ORDER BY `population` DESC, `country_name` ASC
+LIMIT 30;
+
+-- 23. Countries and Currency (Euro / Not Euro)
+SELECT `country_name`, `country_code`,
+IF(`currency_code` = 'EUR', 'Euro', 'Not Euro') AS `currency`
+FROM `countries`
+ORDER BY `country_name`;
+
+-- 24. All Diablo Characters
+SELECT `name` FROM `characters`
+ORDER BY `name` ASC;
