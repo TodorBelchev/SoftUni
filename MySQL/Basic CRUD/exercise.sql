2476b034af261c268c86f4f8a571221465dab402
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-27 17:54:46 +0300

    Added solution

--- MySQL/Basic CRUD/exercise.sql
+++ MySQL/Basic CRUD/exercise.sql
@@ -67,6 +67,34 @@ SELECT * FROM `employees`
 ORDER BY `salary` DESC, `first_name` ASC, `last_name` DESC, `middle_name` ASC;
 
 -- 15. Create View Employees with Salaries
-CREATE VIEW `v_employees_salaries` AS (
-		SELECT `first_name`, `last_name`, `salary` FROM `employees`
-);
+CREATE VIEW `v_employees_salaries` AS
+SELECT `first_name`, `last_name`, `salary`
+FROM `employees`;
+
+-- 16. Create View Employees with Job Titles
+CREATE VIEW `v_employees_job_titles` AS
+SELECT CONCAT_WS(' ', `first_name`, `middle_name`, `last_name`) AS 'full_name', `job_title`
+FROM `employees`;
+
+-- 17. Distinct Job Titles
+SELECT DISTINCT `job_title`
+FROM `employees`
+ORDER BY `job_title` ASC;
+
+-- 18. Find First 10 Started Projects
+SELECT * FROM `projects`
+ORDER BY `start_date` ASC, `name` ASC, `project_id` ASC
+LIMIT 10;
+
+-- 19. Last 7 Hired Employees
+SELECT `first_name`, `last_name`, `hire_date`
+FROM `employees`
+ORDER BY `hire_date` DESC
+LIMIT 7;
+
+-- 20. Increase Salaries
+UPDATE `employees`
+SET `salary` = `salary` * 1.12
+WHERE `department_id` IN(1, 2, 4, 11);
+
+SELECT `salary` FROM `employees`;
