c406700c852f22982463cc2148c4cbd8980eadd4
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-05-31 17:16:07 +0300

    Fixed solution

--- MySQL/Built-in functions/exercise.sql
+++ MySQL/Built-in functions/exercise.sql
@@ -105,6 +105,6 @@ ORDER BY `name`;
 
 -- 16. Orders Table
 SELECT `product_name`, `order_date`,
-ADDDATE(`order_date`, 3) AS `pay_due`,
-ADDDATE(`order_date`, 30) AS `deliver_due`
+DATE_ADD(`order_date`, INTERVAL 3 DAY) AS 'pay_due',
+DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due'
 FROM `orders`;
