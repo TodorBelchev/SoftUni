cd44eabd9208be5d254727787ffeeeefd462117d
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-04 08:57:03 +0300

    Added solution

--- MySQL/Table Relations/exercise.sql
+++ MySQL/Table Relations/exercise.sql
@@ -128,3 +128,115 @@ PRIMARY KEY(`teacher_id`),
 ADD CONSTRAINT `fk_teachers_managers`
 FOREIGN KEY (`manager_id`)
 REFERENCES `teachers`(`teacher_id`);
+
+-- 5. Online Store Database
+CREATE DATABASE `online_store`;
+USE `online_store`;
+
+CREATE TABLE `item_types`(
+	`item_type_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `items`(
+	`item_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50),
+    `item_type_id` INT(11),
+    CONSTRAINT `fk_items_item_types`
+    FOREIGN KEY (`item_type_id`)
+    REFERENCES `item_types`(`item_type_id`)
+);
+
+CREATE TABLE `cities`(
+	`city_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `customers`(
+	`customer_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50),
+    `birthday` DATE,
+    `city_id` INT(11),
+    CONSTRAINT `fk_customers_cities`
+    FOREIGN KEY(`city_id`)
+    REFERENCES `cities`(`city_id`)
+);
+
+CREATE TABLE `orders`(
+	`order_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11),
+    CONSTRAINT `fk_orders_customers`
+    FOREIGN KEY (`customer_id`)
+    REFERENCES `customers`(`customer_id`)
+);
+
+CREATE TABLE `order_items`(
+	`order_id` INT(11),
+    `item_id` INT(11),
+    CONSTRAINT `pk_order_items`
+    PRIMARY KEY(`order_id`, `item_id`),
+    CONSTRAINT `fk_order_items_orders`
+    FOREIGN KEY(`order_id`)
+    REFERENCES `orders`(`order_id`),
+    CONSTRAINT `fk_order_items_items`
+    FOREIGN KEY(`item_id`)
+    REFERENCES `items`(`item_id`)
+);
+
+-- 6. University Database
+CREATE DATABASE `university`;
+USE `university`;
+
+CREATE TABLE `subjects`(
+	`subject_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `subject_name` VARCHAR(50)
+);
+
+CREATE TABLE `majors`(
+	`major_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+
+CREATE TABLE `students`(
+	`student_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `student_number` VARCHAR(12),
+    `student_name` VARCHAR(50),
+    `major_id` INT(11),
+    CONSTRAINT `fk_students_majors`
+    FOREIGN KEY(`major_id`)
+    REFERENCES `majors`(`major_id`)
+);
+
+CREATE TABLE `payments`(
+	`payment_id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `payment_date` DATE,
+    `payment_amount` DECIMAL(8,2),
+    `student_id` INT(11),
+    CONSTRAINT `fk_payments_students`
+    FOREIGN KEY(`student_id`)
+    REFERENCES `students`(`student_id`)
+);
+
+CREATE TABLE `agenda`(
+	`student_id` INT(11),
+    `subject_id` INT(11),
+    CONSTRAINT `fk_agenda_students`
+    FOREIGN KEY(`student_id`)
+    REFERENCES `students`(`student_id`),
+    CONSTRAINT `fk_agenda_subjects`
+    FOREIGN KEY(`subject_id`)
+    REFERENCES `subjects`(`subject_id`)
+);
+
+-- 7. Peaks in Rila
+SELECT 
+    `mountain_range`,
+    `peak_name`,
+    `elevation` AS `peak_elevation`
+FROM
+    `mountains`
+        JOIN
+    `peaks` ON `mountains`.`id` = `peaks`.`mountain_id`
+WHERE
+    `mountains`.`mountain_range` = 'Rila'
+ORDER BY `peaks`.`elevation` DESC;
