f39b18f9587a673dc76d486aa48d43495b47f3db
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-08 15:54:17 +0300

    Added solution

--- MySQL/Subqueries and JOINs/exercise.sql
+++ MySQL/Subqueries and JOINs/exercise.sql
@@ -114,3 +114,58 @@ FROM
 WHERE
     e.`employee_id` = 24
 ORDER BY p.`name` ASC;
+
+-- 09. Employee Manager
+SELECT 
+    e.`employee_id`,
+    e.`first_name`,
+    e.`manager_id`,
+    e2.`first_name` AS `manager_name`
+FROM
+    `employees` AS e
+        JOIN
+    `employees` AS e2 ON e.`manager_id` = e2.`employee_id`
+WHERE
+    e2.`employee_id` IN (3 , 7)
+ORDER BY e.`first_name` ASC;
+
+-- 10. Employee Summary
+SELECT 
+    e.`employee_id`,
+    CONCAT(e.`first_name`, ' ', e.`last_name`) AS `employee_name`,
+    CONCAT(e2.`first_name`, ' ', e2.`last_name`) AS `manager_name`,
+    d.`name` AS `department_name`
+FROM
+    `employees` AS e
+        JOIN
+    `employees` AS e2 ON e.`manager_id` = e2.`employee_id`
+        JOIN
+    `departments` AS d ON e.`department_id` = d.`department_id`
+ORDER BY e.`employee_id` ASC
+LIMIT 5;
+
+-- 11. Min Average Salary
+SELECT 
+    AVG(`employees`.`salary`) AS `min_average_salary`
+FROM
+    `employees`
+GROUP BY `department_id`
+ORDER BY `min_average_salary`
+LIMIT 1;
+
+-- 12. Highest Peaks in Bulgaria
+SELECT 
+    mc.`country_code`,
+    m.`mountain_range`,
+    p.`peak_name`,
+    p.`elevation`
+FROM
+    `mountains` AS m
+        JOIN
+    `peaks` AS p ON m.`id` = p.`mountain_id`
+        JOIN
+    `mountains_countries` AS mc ON m.`id` = mc.`mountain_id`
+WHERE
+    mc.`country_code` = 'BG'
+        AND p.`elevation` > 2835
+ORDER BY p.`elevation` DESC;
