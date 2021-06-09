6d2f0782b9ff8c4677c7e883d6ce1e72060bb72e
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-09 17:03:15 +0300

    Added solution

--- MySQL/Exam preps/09.06.2019/exam.sql
+++ MySQL/Exam preps/09.06.2019/exam.sql
@@ -80,3 +80,67 @@ WHERE
 -- 4. Delete
 DELETE FROM `employees`
 WHERE `id` NOT IN (SELECT `employee_id` FROM `employees_clients`);
+
+-- 5. Clients
+SELECT `id`, `full_name`
+FROM `clients`
+ORDER BY `id` ASC;
+
+-- 6. Newbies
+SELECT 
+    `id`,
+    CONCAT(`first_name`, ' ', `last_name`) AS `full_name`,
+    CONCAT('$', `salary`) AS `salary`,
+    `started_on`
+FROM
+    `employees`
+WHERE
+    `salary` >= 100000
+        AND DATE(`started_on`) >= '2018-01-01'
+ORDER BY `salary` DESC , `id` ASC;
+
+-- 7. Cards against Humanity
+SELECT 
+    ca.`id`,
+    CONCAT(ca.`card_number`, ' : ', c.`full_name`) AS `card_token`
+FROM
+    `clients` AS c
+        JOIN
+    `bank_accounts` AS ba ON c.`id` = ba.`client_id`
+        JOIN
+    `cards` AS ca ON ba.`id` = ca.`bank_account_id`
+ORDER BY ca.`id` DESC;
+
+-- 8. Top 5 employees
+SELECT 
+    CONCAT(`first_name`, ' ', `last_name`) AS `name`,
+    `started_on`,
+    `count_of_clients`
+FROM
+    `employees` AS e
+        JOIN
+    (SELECT 
+        `employee_id`, COUNT(`client_id`) AS `count_of_clients`
+    FROM
+        `employees_clients`
+    GROUP BY `employee_id`) AS c ON e.`id` = c.`employee_id`
+ORDER BY `count_of_clients` DESC , `employee_id` ASC
+LIMIT 5;
+
+-- 9. Branch cards
+SELECT 
+    b.`name`, COUNT(ca.`id`) AS `count_of_cards`
+FROM
+    `branches` AS b
+        LEFT JOIN
+    `employees` AS e ON b.`id` = e.`branch_id`
+        LEFT JOIN
+    `employees_clients` AS ec ON e.`id` = ec.`employee_id`
+        LEFT JOIN
+    `clients` AS c ON ec.`client_id` = c.`id`
+        LEFT JOIN
+    `bank_accounts` AS ba ON c.`id` = ba.`client_id`
+        LEFT JOIN
+    `cards` AS ca ON ba.`id` = ca.`bank_account_id`
+GROUP BY b.`name`
+ORDER BY `count_of_cards` DESC , b.`name` ASC;
