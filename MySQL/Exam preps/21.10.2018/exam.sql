5248b1967ba1a22533e5c1e7873a1efb0930e95c
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-10 08:57:11 +0300

    Added solution

--- MySQL/Exam preps/21.10.2018/exam.sql
+++ MySQL/Exam preps/21.10.2018/exam.sql
@@ -56,3 +56,42 @@ CREATE TABLE `travel_cards`(
     FOREIGN KEY (`journey_id`)
     REFERENCES `journeys`(`id`)
 );
+
+-- 2. Insert
+INSERT INTO `travel_cards`
+	(`card_number`,
+	`job_during_journey`,
+	`colonist_id`,
+	`journey_id`)
+(SELECT 
+	CASE
+		WHEN `birth_date` > '1980-01-01'
+			THEN CONCAT(YEAR(`birth_date`), DAY(`birth_date`), LEFT(`ucn`, 4))
+            ELSE CONCAT(YEAR(`birth_date`), MONTH(`birth_date`), RIGHT(`ucn`, 4))
+		END,
+	CASE
+		WHEN `id` % 2 = 0 THEN 'Pilot'
+        WHEN `id` % 3 = 0 THEN 'Cook'
+        ELSE 'Engineer'
+	END,
+    `id`,
+    LEFT(`ucn`, 1)
+	FROM `colonists`
+	WHERE `id` BETWEEN 96 AND 100);
+
+-- 3. Update    
+UPDATE `journeys`
+SET `purpose` =
+	CASE
+		WHEN `id` % 2 = 0 THEN 'Medical'
+		WHEN `id` % 3 = 0 THEN 'Technical'
+		WHEN `id` % 5 = 0 THEN 'Educational'
+		WHEN `id` % 7 = 0 THEN 'Military'
+		ELSE `purpose`
+	END;
+
+-- 4. Delete
+DELETE FROM `colonists`
+WHERE `id` NOT IN (
+	SELECT `colonist_id` FROM `travel_cards`
+);
