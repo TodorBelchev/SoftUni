7d8f055f8f4ab53746a47e319309924d1d45eb74
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-11 09:27:55 +0300

    Added auth controller and login/register views

--- JavaScript Backend/Cubicle/config/routes.js
+++ JavaScript Backend/Cubicle/config/routes.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const homeController = require('../controllers/homeController');
 const cubeController = require('../controllers/cubeController');
 const accessoryController = require('../controllers/accessoryController');
+const authController = require('../controllers/authController');
 const notFoundController = require('../controllers/notFoundController');
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/', homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use('/auth', authController);
 router.get('*', notFoundController);
 
 module.exports = router;
---
+++ JavaScript Backend/Cubicle/controllers/authController.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+module.exports = router;
