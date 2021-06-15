eb521aab0ecf9058ca90a69bd76b8744507b9c66
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-15 11:26:47 +0300

    Added like functionality

--- JavaScript Backend/Exam preps/Theaters/controllers/playController.js
+++ JavaScript Backend/Exam preps/Theaters/controllers/playController.js
@@ -13,7 +13,8 @@ router.post('/create', async (req, res) => {
             title: req.body.title,
             description: req.body.description,
             imageUrl: req.body.imageUrl,
-            isPublic: req.body.isPublic ? true : false
+            isPublic: req.body.isPublic ? true : false,
+            creator: req.user._id
         }
         const play = await playService.create(data);
         res.redirect(`/play/${play._id}/details`);
@@ -30,7 +31,16 @@ router.get('/:id/details', async (req, res) => {
         isCreator: req.user && play.creator == req.user._id,
         isLiked: req.user && play.usersLiked.some(x => x._id == req.user._id)
     }
+    console.log(req.user);
+    console.log(play.creator);
     res.render('details', ctx);
+});
+
+router.get('/:id/like', async (req, res) => {
+    const id = req.params.id;
+    await playService.like(id, req.user._id);
+    
+    res.redirect(`/play/${id}/details`);
 });
 
 module.exports = router;
--- JavaScript Backend/Exam preps/Theaters/services/playService.js
+++ JavaScript Backend/Exam preps/Theaters/services/playService.js
@@ -1,20 +1,27 @@
 const Play = require("../models/Play");
 
-const create =  (data) => {
+const create = (data) => {
     const play = new Play(data);
     return play.save();
 };
 
 const getById = async (id) => {
     return await Play.findById(id).lean();
-}
+};
 
 const getAll = async () => {
-    return await Play.find({}).sort({ createdAt: 'desc'}).limit(3).lean();
-}
+    return await Play.find({}).sort({ createdAt: 'desc' }).limit(3).lean();
+};
+
+const like = async (id, userId) => {
+    const play = await Play.findById(id);
+    play.usersLiked.push(userId);
+    return play.save();
+};
 
 module.exports = {
     create,
     getById,
-    getAll
+    getAll,
+    like
 }
