a82789981943ffbb3d5794318803645b749519e5
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-16 11:16:02 +0300

    Added solution

--- MySQL/Exam preps/18.10.2020/exam.sql
+++ MySQL/Exam preps/18.10.2020/exam.sql
@@ -80,3 +80,56 @@ CREATE TABLE `employees`(
     FOREIGN KEY (`manager_id`)
     REFERENCES `employees`(`id`)
 );
+
+-- 2. Insert
+INSERT INTO `products_stores`
+(`product_id`, `store_id`)
+SELECT `id`, 1 FROM `products`
+WHERE `id` NOT IN (SELECT `product_id` FROM `products_stores`);
+
+-- 3. Update
+SET foreign_key_checks = 0;
+UPDATE `employees` AS e
+LEFT JOIN `stores` AS s ON e.`store_id` = s.`id`
+SET e.`manager_id` = 3 AND e.`salary` = e.`salary` - 500
+WHERE s.`name` NOT IN ('Cardguard', 'Veribet') AND YEAR(DATE(e.`hire_date`)) > 2003;
+
+-- 4. Delete
+DELETE FROM `employees` AS e
+WHERE (e.`manager_id` IS NOT NULL AND e.`manager_id` != e.`id`) AND e.`salary` >= 6000;
+
+-- 5. Employees
+SELECT `first_name`, `middle_name`, `last_name`, `salary`, `hire_date`
+FROM `employees`
+ORDER BY `hire_date` DESC;
+
+-- 6. Products with old pictures
+SELECT 
+    p.`name` AS `product_name`,
+    p.`price`,
+    p.`best_before`,
+    CONCAT(LEFT(p.`description`, 10), '...') AS `short_description`,
+    pic.`url`
+FROM
+    `products` AS p
+        JOIN
+    `pictures` AS pic ON p.`picture_id` = pic.`id`
+WHERE
+    CHAR_LENGTH(p.`description`) > 100
+        AND YEAR(DATE(pic.`added_on`)) < 2019
+        AND p.`price` > 20
+ORDER BY p.`price` DESC;
+
+-- 7. Counts of products in stores
+SELECT 
+    s.`name`,
+    COUNT(p.`id`) AS `product_count`,
+    ROUND(AVG(p.`price`), 2) AS `avg`
+FROM
+    `stores` AS s
+        LEFT JOIN
+    `products_stores` AS ps ON s.`id` = ps.`store_id`
+        LEFT JOIN
+    `products` AS p ON ps.`product_id` = p.`id`
+GROUP BY s.`name`
+ORDER BY `product_count` DESC , `avg` DESC , s.`id` ASC;
