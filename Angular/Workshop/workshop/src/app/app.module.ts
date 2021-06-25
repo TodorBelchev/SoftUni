55da287fe919121b61b8031b2a8371eedbe7c6a4
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-25 11:00:19 +0300

    Added footer

--- Angular/Workshop/workshop/src/app/app.module.ts
+++ Angular/Workshop/workshop/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
