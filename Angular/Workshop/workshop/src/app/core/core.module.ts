031d4cb5ad2e8628518f121b0d1161e6f6ad5734
Todor Belchev <71307782+TodorBelchev@users.noreply.github.com>
2021-06-25 10:57:47 +0300

    Added header

--- Angular/Workshop/workshop/src/app/core/core.module.ts
+++ Angular/Workshop/workshop/src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
