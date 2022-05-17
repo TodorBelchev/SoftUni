37f61b76ae461745c96241fe900ca381e7e748e9
Todor Belchev <todorbelchev@live.com>
2022-05-17 20:26:06 +0300

    Initial commit.

---
+++ Java Web/Spring fundamentals/Mobilelele/src/main/java/com/softuni/Mobilelele/MobileleleApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.Mobilelele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileleleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileleleApplication.class, args);
+	}
+
+}
---
+++ Java Web/Spring fundamentals/Mobilelele/src/test/java/com/softuni/Mobilelele/MobileleleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.Mobilelele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
