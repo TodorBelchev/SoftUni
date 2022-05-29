33e771bc74c0eb37906f7fb796515970c62f1fd4
Todor Belchev <todorbelchev@live.com>
2022-05-29 11:51:18 +0300

    Extracted common html fragments.

---
+++ Java Web/Spring fundamentals/Exam preps/CoffeeShop/src/main/java/com/softuni/CoffeeShop/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.softuni.CoffeeShop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register";
+    }
+}
