a5a0664da74601499010af29085ca3c9c8203c2c
Todor Belchev <todorbelchev@live.com>
2022-06-04 15:44:46 +0300

    Initial commit

---
+++ Java Web/Spring fundamentals/Exam preps/Judge/src/main/java/com/softuni/Judge/JudgeApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.Judge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgeApplication.class, args);
+	}
+
+}
