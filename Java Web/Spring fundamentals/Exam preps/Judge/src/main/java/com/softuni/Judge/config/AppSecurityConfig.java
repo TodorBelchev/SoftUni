8519f36b95b8bed26ae8a65a2a9058207d86e0f2
Todor Belchev <todorbelchev@live.com>
2022-06-05 07:24:48 +0300

    Added security config.

---
+++ Java Web/Spring fundamentals/Exam preps/Judge/src/main/java/com/softuni/Judge/config/AppSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.softuni.Judge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/", "/users/login", "/users/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/users/login")
+                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                .passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
+                .defaultSuccessUrl("/")
+                .failureForwardUrl("/users/login")
+                .and()
+                .logout().logoutUrl("/users/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+
+        return http.build();
+    }
+}
