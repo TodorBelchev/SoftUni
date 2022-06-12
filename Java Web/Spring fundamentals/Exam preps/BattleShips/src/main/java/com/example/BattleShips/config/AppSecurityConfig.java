5b236f49b6fc18ba91139d15d088c2f9d3fe5646
Todor Belchev <todorbelchev@live.com>
2022-06-12 08:08:39 +0300

    Extracted html fragments

--- Java Web/Spring fundamentals/Exam preps/BattleShips/src/main/java/com/example/BattleShips/config/AppSecurityConfig.java
+++ Java Web/Spring fundamentals/Exam preps/BattleShips/src/main/java/com/example/BattleShips/config/AppSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.example.BattleShips.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class AppSecurityConfig {
 
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/js/**", "/css/**", "/images/**").permitAll()
---
+++ Java Web/Spring fundamentals/Exam preps/BattleShips/src/main/java/com/example/BattleShips/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.BattleShips.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        if (currentPrincipalName.equals("anonymousUser")) {
+            return "index";
+        }
+
+        return "home";
+    }
+}
