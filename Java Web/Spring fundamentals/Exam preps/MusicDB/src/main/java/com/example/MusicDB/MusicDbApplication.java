ca4f17e45419c57f5e7dfef39ffe10522932b4d9
Todor Belchev <todorbelchev@live.com>
2022-06-13 20:06:06 +0300

    Initial commit

---
+++ Java Web/Spring fundamentals/Exam preps/MusicDB/src/main/java/com/example/MusicDB/MusicDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicDbApplication.class, args);
+	}
+
+}
---
+++ Java Web/Spring fundamentals/Exam preps/MusicDB/src/test/java/com/example/MusicDB/MusicDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
